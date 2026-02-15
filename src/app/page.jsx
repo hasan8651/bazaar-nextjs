d94b2d8a82735a6fe898f602c70e0dd6e73d11a8
Mahmudul Hasan <hasan865@gmail.com>
2026-02-15 12:23:27 +0600

    starting the project

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-6xl font-bold text-orange-500">
+        Hello Bazaar!
+      </h1>
+    </div>
+  )
+}
