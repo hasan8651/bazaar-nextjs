da8a48dc5ddb328d10574d0843b62c0cba7aa5bd
hasan8651 <hasan865@gmail.com>
2026-02-19 03:57:46 +0600

    MODIFIED campaign text into two rows & alignment to top  in hero section

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -1,25 +1,29 @@
 "use client";
-import {motion, AnimatePresence } from 'framer-motion';
-import { BadgeCheck, ShieldCheck, Truck } from 'lucide-react';
-import Image from 'next/image';
-import { useEffect, useState } from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import { BadgeCheck, ShieldCheck, Truck } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const slides = [
   {
     image: "https://i.ibb.co/dwzd0vW8/slide1.webp",
-    campaign: "Wear the Trend. Be the Trend. Upgrade Your Style with Our New Arrivals.",
+    campaign: `Wear the Trend. Be the Trend.
+    Upgrade Your Style with Our New Arrivals.`,
   },
   {
     image: "https://i.ibb.co/cKLV5xwB/slide2.webp",
-    campaign: "Mega Tech Sale: Power Up for Less! Premium Tech. Unbeatable Prices.",
+    campaign: `Mega Tech Sale: Power Up for Less!
+    Premium Tech. Unbeatable Prices.`,
   },
   {
     image: "https://i.ibb.co/xKK2X8wZ/slide3.webp",
-    campaign: "Farm Fresh Goodness, Delivered to Your Doorstep. Eat Fresh, Live Healthy.",
+    campaign: `Farm Fresh Goodness, Delivered to Your Doorstep.
+     Eat Fresh, Live Healthy.`,
   },
   {
     image: "https://i.ibb.co/R422PVq2/slide4.webp",
-    campaign: "Premium Furniture. Affordable Prices. Where Comfort Meets Style.",
+    campaign: `Premium Furniture. Affordable Prices.
+    Where Comfort Meets Style.`,
   },
 ];
 
@@ -40,8 +44,8 @@ export default function Hero() {
   const currentSlide = slides[currentIndex];
 
   return (
-   <section className="relative w-full h-[80vh] text-white overflow-hidden">
-       <div className="absolute inset-0">
+    <section className="relative w-full h-[80vh] text-white overflow-hidden">
+      <div className="absolute inset-0">
         <AnimatePresence mode="wait">
           <motion.div
             key={currentSlide.image}
@@ -91,43 +95,53 @@ export default function Hero() {
       <div className="absolute inset-0 bg-gradient-to-b from-indigo-900/70 via-blue-900/40 to-black/70" />
 
       {/* Foreground content */}
-      <div className="relative z-10 max-w-7xl mx-auto h-full px-4 py-6 md:py-4 flex items-center">
-        <div className="w-full md:w-2/3 lg:w-1/2">
+      <div className="relative z-10 max-w-7xl mx-auto h-full px-4 pt-6">
+        <div className="w-full md:w-2/3">
           {/* Campaign (changes per slide) */}
-          <div className="inline-flex items-center rounded-full bg-white/10 px-3 py-1 font-medium text-(--accent) backdrop-blur-sm border border-indigo-500/20">
-            <span className="mr-2 h-1.5 w-1.5 rounded-full bg-indigo-400" />
-            <span>{currentSlide.campaign}</span>
+          <div className="w-fit rounded-full bg-white/10 px-8 py-1 md:text-3xl font-medium md:font-extrabold text-(--accent) backdrop-blur-sm border border-indigo-500/20">
+            <span className="whitespace-pre-line leading-tight">
+              {currentSlide.campaign}
+            </span>
           </div>
 
           {/* Main Heading */}
-          <h1 className="mt-4 text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+          <h1 className="mt-12 text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
             Development Only.
           </h1>
 
           {/* Subheading */}
           <p className="mt-4 text-base md:text-xl text-primary max-w-xl">
-             Development Only.
+            Development Only.
           </p>
 
           {/* CTA need update */}
-         <div className="mt-8 grid grid-cols-2 gap-4 w-2/3">
-          <button className="inline-flex items-center justify-center rounded-full border border-(--secondary) bg-indigo-500/10 py-3 text-sm md:text-lg font-semibold text-white backdrop-blur-sm transition hover:bg-(--secondary) cursor-pointer">
-            Shop Now
+          <div className="mt-8 md:mt-16 grid grid-cols-2 gap-4 w-2/3">
+            <button className="inline-flex items-center justify-center rounded-full border border-(--secondary) bg-indigo-500/10 py-3 text-sm md:text-lg font-semibold text-white backdrop-blur-sm transition hover:bg-(--secondary) cursor-pointer">
+              Shop Now
             </button>
             <button className="inline-flex items-center justify-center rounded-full border border-(--secondary) bg-indigo-500/10 py-3 text-sm md:text-lg font-semibold text-white backdrop-blur-sm transition hover:bg-(--secondary) cursor-pointer">
-View Best Sellers
-</button> 
-</div>
- </div>
-          {/* Trust badges */}
-            <div className="absolute bottom-10 left-4 right-4 z-20">
-          <div className="mt-6 flex flex-col md:flex-row gap-2 md:gap-6 text-sm font-medium opacity-90">
-            <div className="flex items-center gap-2"><ShieldCheck className="w-5 h-5 text-green-600" /> Secure checkout</div>
-            <div className="flex items-center gap-2"> <Truck className="w-5 h-5 text-blue-600" /> Fast delivery across Bangladesh</div>
-            <div className="flex items-center gap-2"><BadgeCheck className="w-5 h-5 text-orange-500" /> Safety-tested products only</div>
+              View Best Sellers
+            </button>
           </div>
         </div>
-       </div>
+        {/* Trust badges */}
+        <div className="absolute bottom-2 left-4 right-4 z-20">
+          <div className="mt-6 flex flex-col md:flex-row gap-2 md:gap-6 text-sm font-medium opacity-90">
+            <div className="flex items-center gap-2">
+              <ShieldCheck className="w-5 h-5 text-green-600" /> Secure checkout
+            </div>
+            <div className="flex items-center gap-2">
+              {" "}
+              <Truck className="w-5 h-5 text-blue-600" /> Fast delivery across
+              Bangladesh
+            </div>
+            <div className="flex items-center gap-2">
+              <BadgeCheck className="w-5 h-5 text-orange-500" /> Safety-tested
+              products only
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
