8fc1b0e606b5a388e5b9f6411a191115297d07bf
akashsarker2478 <akashsarker2478@gmail.com>
2026-02-18 11:16:22 +0600

    [feat]: integrated Navbar into page.jsx to show it on UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
+import Navbar from './Components/Navbar';
+
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center">
-      <h1 className="text-6xl font-bold text-orange-500">
-        Hello Bazaar!
-      </h1>
-      <h1 className="mt-4 text-2xl text-blue-500">Development Branch</h1>
-    </div>
-  )
+    <main>
+     
+      <Navbar /> 
+
+      <div className="flex flex-col min-h-[80vh] items-center justify-center">
+        <h1 className="text-6xl font-bold text-orange-500">
+          Hello Bazaar!
+        </h1>
+        <h1 className="mt-4 text-2xl text-blue-500">Development Branch</h1>
+      </div>
+    </main>
+  );
 }
