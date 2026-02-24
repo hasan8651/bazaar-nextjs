1d7690477a561521bd762c8e831ffc759e79a467
Mahmudul Hasan <hasan865@gmail.com>
2026-02-24 12:37:12 +0600

    MODIFIED brand logo added in loading page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 import Footer from './Components/Footer';
 import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
+import Loading from './loading';
 export default function Home() {
   return (
     <main>
-      <Navbar /> 
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
         <h1 className="text-6xl font-bold text-orange-500">
           Feature/Top Selling Products
@@ -13,42 +14,40 @@ export default function Home() {
         <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
       </div>
 
-         <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+      <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
         <h1 className="text-6xl font-bold text-orange-500">
           Upcoming/ Pre-Book Products
         </h1>
         <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
       </div>
 
-         <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+      <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
         <h1 className="text-3xl font-bold text-orange-500">
           12 Products from different Categories with CTA
         </h1>
         <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
       </div>
 
-         <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+      <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
         <h1 className="text-6xl font-bold text-orange-500">
           TOP BRANDS LOGO CAROUSEL
         </h1>
         <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
       </div>
-      
-   <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+
+      <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
         <h1 className="text-6xl font-bold text-orange-500">
           TESTIMONIAL / USER REVIEW
         </h1>
         <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
       </div>
 
-
-
-      <div>
-        <Footer/>
-      </div>
  
+        <Footer />
+    
+
     </main>
-    
-    
+
+
   );
 }
