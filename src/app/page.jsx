51e12f4c0bbad875c643917600cda05910afd7aa
Mahmudul Hasan <hasan865@gmail.com>
2026-02-24 11:43:35 +0600

    MODIFIED Home page section wise space reserved

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,40 @@ export default function Home() {
     <main>
       <Navbar /> 
       <Hero/>
- 
+      <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+        <h1 className="text-6xl font-bold text-orange-500">
+          Feature/Top Selling Products
+        </h1>
+        <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
+      </div>
+
+         <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+        <h1 className="text-6xl font-bold text-orange-500">
+          Upcoming/ Pre-Book Products
+        </h1>
+        <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
+      </div>
+
+         <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+        <h1 className="text-3xl font-bold text-orange-500">
+          12 Products from different Categories with CTA
+        </h1>
+        <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
+      </div>
+
+         <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+        <h1 className="text-6xl font-bold text-orange-500">
+          TOP BRANDS LOGO CAROUSEL
+        </h1>
+        <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
+      </div>
+      
+   <div className="flex flex-col h-50 items-center justify-center border-2 border-dashed border-indigo-600 m-2">
+        <h1 className="text-6xl font-bold text-orange-500">
+          TESTIMONIAL / USER REVIEW
+        </h1>
+        <h1 className="mt-4 text-2xl text-blue-500">Under Development</h1>
+      </div>
 
 
 
